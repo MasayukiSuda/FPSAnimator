1ed64b1a300dead125ea1845e8c47468b1221de1
MasayukiSuda <topman31daasuu@gmail.com>
2016-01-19 01:03:17 +0900

    small fix

--- library/src/main/java/com/daasuu/library/DisplayObject.java
+++ library/src/main/java/com/daasuu/library/DisplayObject.java
@@ -7,10 +7,8 @@ import com.daasuu.library.animator.TweenAnimator;
 
 /**
  * DisplayObject class.
- * <p/>
  * When you only use default animation and drawing class which is provided by this library,
  * you can use simple interface to compose DisplayObject by calling {@link #with(Drawer)}.
- * <p/>
  * If you need to use your custom animation or drawing class which implements {@link Animator} or {@link Drawer},
  * you prepare their instances and simply set by calling {@link #animator(Animator)}.
  */
